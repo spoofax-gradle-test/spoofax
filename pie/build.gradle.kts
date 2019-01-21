68a7b9b7aa30f61c02def76cf1a8b88ed63653d8
Gabriel Konat <gabrielkonat@gmail.com>
2019-01-21 15:59:10 +0100

    Update to Metaborg Gradle config 0.5.0.

--- pie/build.gradle.kts
+++ pie/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+  id("org.metaborg.gradle.config.java-library")
+}
+
 dependencies {
   api(project(":spoofax.api"))
   api("org.metaborg:pie.runtime:develop-SNAPSHOT")
--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+  id("org.metaborg.gradle.config.java-library")
+}
+
 dependencies {
   api(project(":spoofax.api"))
   api("org.metaborg:log.api:develop-SNAPSHOT")
